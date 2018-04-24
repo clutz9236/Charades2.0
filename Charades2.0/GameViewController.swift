2d9448c9e731f84205aec6f858fa8030d3875bc7
rebert0090 <rebert0090@stu.d214.org>
2018-04-24 11:24:43 -0500

    button created on game view controller

--- Charades2.0/GameViewController.swift
+++ Charades2.0/GameViewController.swift
@@ -9,27 +9,31 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+  
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        button.setTitle("start game", for: .normal)
 
-        // Do any additional setup after loading the view.
+        
     }
 
+  
+  
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
 
-    /*
-    // MARK: - Navigation
+    
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+
 
 }
