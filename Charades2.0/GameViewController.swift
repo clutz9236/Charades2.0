d3e4b8dc611eba370310b6d4022d42ad7f2e0fe7
rebert0090 <rebert0090@stu.d214.org>
2018-04-24 11:35:53 -0500

    deleted thing

--- Charades2.0/GameViewController.swift
+++ Charades2.0/GameViewController.swift
@@ -15,9 +15,6 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-        button.setTitle("start game", for: .normal)
-
         
     }
 
