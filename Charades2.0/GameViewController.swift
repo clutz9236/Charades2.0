cf2b2a0b4170107088cab61f1958a85fbc55734b
Ryan <rkohn1176@stu.d214.org>
2018-04-24 11:29:28 -0500

    array was made

--- Charades2.0/GameViewController.swift
+++ Charades2.0/GameViewController.swift
@@ -15,12 +15,18 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let AnimalWords =  ["llama", "dog", "mockingbird", "fly", "parrot", "sheep", "coyote", "lion", "zebra", "cheetah", "polar bear", "bear", "owl", "tiger", "husky", "panda", "monkey", "penguin", "peacock", "fox", "dolphin", "deer", "chicken", "turkey", "pig", "fish", "rhino", "cow", "frog", "bunny", "wolf", "porcupine", "whale", "kangaroo"]
+        
         let button = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         button.setTitle("start game", for: .normal)
 
         
     }
- 
+
+  
+  
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
