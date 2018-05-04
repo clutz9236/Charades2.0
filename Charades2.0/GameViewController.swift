8b24e3ea47e9216abfea81475c7254a3749d751d
rebert0090 <rebert0090@stu.d214.org>
2018-05-04 11:42:09 -0500

    yes

--- Charades2.0/GameViewController.swift
+++ Charades2.0/GameViewController.swift
@@ -10,57 +10,63 @@ import UIKit
 
 class GameViewController: UIViewController {
     
-    @IBOutlet weak var rightAnswerStack: UIStackView!
+
+    @IBOutlet weak var tapStack: UIStackView!
     @IBOutlet weak var startGameOutlet: UIButton!
     @IBOutlet weak var gameLabel: UILabel!
     @IBOutlet weak var TimerLabel: UILabel!
-    @IBOutlet weak var wrongAnswerStack: UIStackView!
-    @IBOutlet var rightAnswerTap: UITapGestureRecognizer!
-    @IBOutlet var wrongAnswerTap: UITapGestureRecognizer!
+    @IBOutlet var tapGestureOutlet: UITapGestureRecognizer!
     
     var counter = 45
     var myTimer: Timer!
     var right = 0
     var wrong = 0
     var countTimesTapped = 0
+    let screenSize = UIScreen.main.bounds
     
+
    let AnimalWords =  ["Llama", "Dog", "Fly", "Parrot", "Sheep", "Coyote", "Lion", "Zebra", "Cheetah", "Polar Bear", "Bear", "Owl", "Tiger", "Husky", "Panda", "Monkey", "Penguin", "Peacock", "Fox", "Dolphin", "Deer", "Chicken", "Turkey", "Pig", "Fish", "Rhino", "Cow", "Frog", "Bunny", "Wolf", "Porcupine", "Whale", "Kangaroo"]
     
     
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        override func viewDidLoad() {
+            super.viewDidLoad()
        
         
         gameLabel.isHidden = true
-        rightAnswerStack.isUserInteractionEnabled = false
-        wrongAnswerStack.isUserInteractionEnabled = false
-        wrongAnswerTap.isEnabled = false
-        rightAnswerTap.isEnabled = false
-        
+        tapGestureOutlet.isEnabled = false
+        tapStack.isUserInteractionEnabled = false
+            
     }
 
-    @IBAction func rightAnswerTapAction(_ sender: UITapGestureRecognizer) {
+    @IBAction func tapGestureRecognizer(_ sender: UITapGestureRecognizer) {
+        var location: CGPoint = sender.location(in: sender.view)
         if sender.state == .ended {
             countTimesTapped += 1
-            right += 1
             newWord()
         }
     }
     
-    
-    @IBAction func wrongAnswerTapAction(_ sender: UITapGestureRecognizer) {
-        if sender.state == .ended {
-            countTimesTapped += 1
-            right += 1
-            wrong += 1
-            newWord()
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        for touch in touches {
+            let location = touch.location(in: view)
+            
+            if(location.x < screenWidth/2){
+                print("Left")
+                wrong += 1
+                
+            }
+            else {
+                print("Right")
+                right += 1
+            }
         }
     }
     
   
-  
-    
     func newWord() {
         if AnimalWords.count > countTimesTapped {
             gameLabel.text = AnimalWords[Int(arc4random_uniform(UInt32(AnimalWords.count)))]
@@ -79,19 +85,17 @@ class GameViewController: UIViewController {
             if self.counter == 0 {
                 self.myTimer.invalidate()
                 self.performSegue(withIdentifier: "ResultsSegue", sender: nil)
-                
+               
             }
             
         }
         gameLabel.text = ("\(AnimalWords[0])")
         startGameOutlet.isHidden = true
         gameLabel.isHidden = false
-        rightAnswerStack.addGestureRecognizer(rightAnswerTap)
-        wrongAnswerStack.addGestureRecognizer(wrongAnswerTap)
-        rightAnswerStack.isUserInteractionEnabled = true
-        rightAnswerStack.isUserInteractionEnabled = true
-        wrongAnswerTap.isEnabled = true
-        rightAnswerTap.isEnabled = true
+        tapGestureOutlet.isEnabled = true
+        tapStack.isUserInteractionEnabled = true
+        tapStack.addGestureRecognizer(tapGestureOutlet)
+
     }
     
     
