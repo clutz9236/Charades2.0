8777ae2ad19d701cfd6e18daecf8690cd55757ba
Amy Drelicharz <adrelicharz1097@jh-mba158-07.d214.org>
2018-05-22 11:03:35 -0500

    made new view controller

---
+++ Charades2.0/AvengersViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AvengersViewController.swift
+//  Charades2.0
+//
+//  Created by Amy Drelicharz on 5/22/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class AvengersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
