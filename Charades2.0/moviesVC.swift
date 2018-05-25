4ce344ebcfda55b39e76499f93b8057f5d6b784d
rebert0090 <rebert0090@stu.d214.org>
2018-05-25 11:45:07 -0500

    new view controllers for categories

--- Charades2.0/moviesVC.swift
+++ Charades2.0/moviesVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class blankViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class moviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var player:AVAudioPlayer = AVAudioPlayer()
     
     
